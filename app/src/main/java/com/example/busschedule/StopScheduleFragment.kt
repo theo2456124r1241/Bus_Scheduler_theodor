14e24af103bee7e75ce9f78011b0df4ac94c64a6
theo2456124r1241 <106807002+theo2456124r1241@users.noreply.github.com>
2023-02-27 15:56:11 +0100

    suivi tuto video 1,2,3,4,5,6,7

--- app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
+++ app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
@@ -19,6 +19,7 @@ package com.example.busschedule
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
